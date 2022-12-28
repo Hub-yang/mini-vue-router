98692379933eddc7e5417576dbd4fce4f557f43b
Mocheng <18830279823@163.com>
2022-12-28 16:07:39 +0800

    实现router-link组件

--- src/mini-router/RouterLink.js
+++ src/mini-router/RouterLink.js
@@ -1,7 +1,24 @@
-import { defineComponent, h } from "vue"
+import { defineComponent, h, unref } from "vue"
 
 export default defineComponent({
-  setup() {
-    return () => h("a", "router-link")
+  props: {
+    to: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props, { slots }) {
+    return () => {
+      // 由于用户传入to属性值可能是字符串也可能是动态的ref，因此做一次脱壳处理
+      const to = unref(props.to)
+      return h(
+        "a",
+        {
+          // 设置a标签的href属性,注意拼接
+          href: "#" + to,
+        },
+        slots.default()
+      )
+    }
   },
 })
